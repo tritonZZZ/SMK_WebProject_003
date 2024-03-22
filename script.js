eb4b4e2ccde23df1bed856cf65d7d4fdb83d5747
Studentas <Studentas@SMK-VIL-208-10.local>
2024-03-22 10:29:20 +0200

    Map generator

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 var container = document.getElementById("container");
 var world = document.getElementById("world");
 
-var x = 0;
-
+var map = [
+//0-x,1-y,2-z, 3-rotX, 4-rotY, 5-rotZ, 6-height, 7-width, 8-color
+	[0,0,0,0,0,0,200,200,"orange"], //0.
+	[200,0,0,0,0,0,200,200,"green"], //1.
+	[400,0,0,0,0,0,200,200,"blue"], //2.
+	[600,0,0,0,0,0,200,200,"pink"], //3.
+];
+ 
 function createWorld(){
-	for(let i = 1; i < 4; i++){
+	for(let i = 0; i < map.length; i++){
 		let myElement = document.createElement("div");
-		myElement.id = "element"+i;
-		myElement.style.position = "abolute";
-		myElement.style.height = 200+"px";
-		myElement.style.width = 200+"px";
-		myElement.style.background = "orange";
+		myElement.id = `element${i}`;
+		myElement.style.position = `absolute`;
+		myElement.style.height = `${map[i][6]}px`;
+		myElement.style.width = `${map[i][7]}px`;
+		myElement.style.background = `${map[i][8]}`;
+		myElement.style.transform = `translate3d(${map[i][0]}px,0px,0px)`;
 		world.append(myElement);
 	}
 }
@@ -19,9 +26,8 @@ createWorld();
 
 function update(){
  	world.style.transform = `
- 		rotateY(${x}deg)
- 		translate3d(500px, 300px, 0px)`;
- 	//x++;
+ 		rotateY(${0}deg)
+ 		translate3d(100px, 50px, 0px)`;
 }
 
  timer = setInterval(update, 10);
