009963edff8aba7ab27527201775c8d50863065b
Studentas <Studentas@SMK-VIL-208-10.local>
2024-03-22 10:47:15 +0200

    Map Generator improved

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ var world = document.getElementById("world");
 
 var map = [
 //0-x,1-y,2-z, 3-rotX, 4-rotY, 5-rotZ, 6-height, 7-width, 8-color
-	[0,0,0,0,0,0,200,200,"orange"], //0.
-	[200,0,0,0,0,0,200,200,"green"], //1.
-	[400,0,0,0,0,0,200,200,"blue"], //2.
-	[600,0,0,0,0,0,200,200,"pink"], //3.
+	[0,0,0,0,0,0,200,200,"orange"], //0. front wall
+	[0,0,-200,0,0,0,200,200,"green"], //1. hinder wall
+	[100,0,100,0,90,0,200,200,"blue"], //2.
+	[100,0,-100,0,90,0,200,200,"violet"],
+	[0,-100,-100,90,0,0,200,200,"brown"], //3.
+	[0,-100,100,90,0,0,200,200,"cyan"],
 ];
- 
+
 function createWorld(){
 	for(let i = 0; i < map.length; i++){
 		let myElement = document.createElement("div");
@@ -17,7 +19,8 @@ function createWorld(){
 		myElement.style.height = `${map[i][6]}px`;
 		myElement.style.width = `${map[i][7]}px`;
 		myElement.style.background = `${map[i][8]}`;
-		myElement.style.transform = `translate3d(${map[i][0]}px,0px,0px)`;
+		myElement.style.opacity = 0.8;
+		myElement.style.transform = `rotateX(${map[i][3]}deg) rotateY(${map[i][4]}deg) rotateZ(${map[i][5]}deg) translate3d(${map[i][0]}px,${map[i][1]}px,${map[i][2]}px)`;
 		world.append(myElement);
 	}
 }
