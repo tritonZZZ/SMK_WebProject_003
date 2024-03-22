3cdac755d4e77e014931bd4b7cad0758f3c69950
Studentas <Studentas@SMK-VIL-208-10.local>
2024-03-22 12:57:38 +0200

    22.03.2024. final

--- script.js
+++ script.js
@@ -4,17 +4,15 @@ var world = document.getElementById("world");
 //variables
 var speed = 7;
 var forward = 0;
+var backward = 0;
+var left = 0;
+var right = 0;
 var dz = 0;
 //---------
 
 var map = [
 //0-x,1-y,2-z, 3-rotX, 4-rotY, 5-rotZ, 6-height, 7-width, 8-color
-	[0,0,0,0,0,0,200,200,"orange"], //0. front wall
-	[0,0,-200,0,0,0,200,200,"green"], //1. hinder wall
-	[100,0,100,0,90,0,200,200,"blue"], //2.
-	[100,0,-100,0,90,0,200,200,"violet"],
-	[0,-100,-100,90,0,0,200,200,"brown"], //3.
-	[0,-100,100,90,0,0,200,200,"cyan"],
+	[0,0,0,90,0,0,200,200,"orange"], //0. front wall
 ];
 
 document.addEventListener("keydown", (event) => {this.move(event, speed)});
@@ -23,6 +21,15 @@ document.addEventListener("keyup", (event) => {this.move(event, 0)});
 function move(ev, vel){
 	if(ev.keyCode == 87){
 		forward = vel;
+	}
+	if(ev.keyCode == 83){
+		backward = vel;
+	}
+	if(ev.keyCode == 65){
+		left = vel;
+	}
+	if(ev.keyCode == 68){
+		right = vel;
 	}
 }
 
@@ -43,7 +50,16 @@ function createWorld(){
 		myElement.style.width = `${map[i][7]}px`;
 		myElement.style.background = `${map[i][8]}`;
 		myElement.style.opacity = 0.8;
-		myElement.style.transform = `rotateX(${map[i][3]}deg) rotateY(${map[i][4]}deg) rotateZ(${map[i][5]}deg) translate3d(${map[i][0]}px,${map[i][1]}px,${map[i][2]}px)`;
+		myElement.style.transform = `
+			rotateX(${map[i][3]}deg) 
+			rotateY(${map[i][4]}deg) 
+			rotateZ(${map[i][5]}deg) 
+			translate3d(
+				${(600 - (map[i][7]/2) + map[i][0])}px, 
+				${(400 - (map[i][6]/2) + map[i][1])}px, 
+				${map[i][2]}px
+			)
+		`;
 		world.append(myElement);
 	}
 }
@@ -51,13 +67,15 @@ function createWorld(){
 createWorld();
 
 function update(){
-	dz = forward;
+	dz = forward - backward;
+	dx = left - right;
 
 	me.z += dz;
+	me.x += dx;
 
  	world.style.transform = `
  		rotateY(${0}deg)
- 		translate3d(500px, 300px, ${me.z}px)`;
+ 		translate3d(${me.x}px, ${me.y}px, ${me.z}px)`;
 }
 
  timer = setInterval(update, 10);
