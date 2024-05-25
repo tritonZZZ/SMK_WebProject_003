f55277efb2bdc7aef32cd87f1d712b81c500495c
Triton <73620841+tritonZZZ@users.noreply.github.com>
2024-05-25 19:29:33 +0300

    Created the game

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 var container = document.getElementById("container");
 var world = document.getElementById("world");
+var menu = document.getElementById("menu");
+var rules = document.getElementById("rules");
 
-//variables
+// Variables
 var deg = Math.PI / 180; // 1 degree in radians because SIN and COS require angles in RADIANS!
 var speed = 7;
 var forward = 0;
@@ -12,49 +14,60 @@ var mouseX = mouseY = 0;
 var dz = 0;
 var lock = false;
 var canlock = true;
-//---------
+var gamePaused = false;
+// ---------
 
 var map = [
-//0-x,1-y,2-z, 3-rotX, 4-rotY, 5-rotZ, 6-height, 7-width, 8-color
-	[0,100,0,90,0,0,2000,2000,"url('textures/grass.jpg')"], //0. floor
-	[0,0,-1000,0,0,0,200,2000,"orange"], //front wall
-	[0,0,1000,0,0,0,200,2000,"url('textures/brick_wall.jpg')"], //hinder wall
-	[-1000,0,0,0,90,0,200,2000,"url('textures/brick_wall.jpg')"], //left wall
-	[1000,0,0,0,90,0,200,2000,"url('textures/brick_wall.jpg')"], //left wall
+	//0-x, 1-y, 2-z, 3-rotX, 4-rotY, 5-rotZ, 6-height, 7-width, 8-color
+	[0, 100, 0, 90, 0, 0, 2000, 2000, "url('textures/metal_floor.jpg')"], // floor
+	[0, -100, 0, 90, 0, 0, 2000, 2000, "url('textures/doom_cieling.png')"], // ceiling
+	[0, 0, -100, 0, 0, 0, 200, 200, "url('textures/enemy2.png')"], // sign
+	[0, 0, -1000, 0, 0, 0, 200, 2000, "url('textures/doom_wall.jpg')"], // front wall
+	[0, 0, 1000, 0, 0, 0, 200, 2000, "url('textures/doom_wall.jpg')"], // hind wall
+	[-1000, 0, 0, 0, 90, 0, 200, 2000, "url('textures/doom_wall.jpg')"], // left wall
+	[1000, 0, 0, 0, 90, 0, 200, 2000, "url('textures/doom_wall.jpg')"], // right wall
 ];
 
-document.addEventListener("mousedown", (event) => {this.move(event, speed)})
-document.addEventListener("keydown", (event) => {this.move(event, speed)});
-document.addEventListener("keyup", (event) => {this.move(event, 0)});
+document.addEventListener("mousedown", (event) => { if (!gamePaused) move(event, speed) });
+document.addEventListener("keydown", (event) => { 
+	if (event.key.toLowerCase() === "m") {
+		activateMenu();
+	} else {
+		if (!gamePaused) move(event, speed);
+	}
+});
+document.addEventListener("keyup", (event) => { if (!gamePaused) move(event, 0) });
 document.addEventListener("mousemove", (event) => {
-	mouseX = event.movementX;
-	mouseY = event.movementY;
+	if (!gamePaused) {
+		mouseX = event.movementX;
+		mouseY = event.movementY;
+	}
 });
 document.addEventListener("pointerlockchange", (event) => {
 	lock = !lock;
 });
-container.onclick = function(){
-	if(!lock && canlock){
+container.onclick = function() {
+	if (!lock && canlock) {
 		container.requestPointerLock();	
 	}
 }
 
-function move(ev, vel){
-	if(ev.keyCode == 87){
+function move(ev, vel) {
+	if (ev.keyCode == 87) {
 		forward = vel;
 	}
-	if(ev.keyCode == 83){
+	if (ev.keyCode == 83) {
 		backward = vel;
 	}
-	if(ev.keyCode == 65){
+	if (ev.keyCode == 65) {
 		left = vel;
 	}
-	if(ev.keyCode == 68){
+	if (ev.keyCode == 68) {
 		right = vel;
 	}
 }
 
-function player(x, y, z, rx, ry, rz){
+function player(x, y, z, rx, ry, rz) {
 	this.x = x;
 	this.y = y;
 	this.z = z;
@@ -65,8 +78,8 @@ function player(x, y, z, rx, ry, rz){
 
 var me = new player(0, 0, 0, 0, 0, 0);
 
-function createWorld(){
-	for(let i = 0; i < map.length; i++){
+function createWorld() {
+	for (let i = 0; i < map.length; i++) {
 		let myElement = document.createElement("div");
 		myElement.id = `element${i}`;
 		myElement.style.position = `absolute`;
@@ -80,7 +93,6 @@ function createWorld(){
 				${(400 - (map[i][6]/2) + map[i][1])}px, 
 				${map[i][2]}px
 			)
-
 			rotateX(${map[i][3]}deg) 
 			rotateY(${map[i][4]}deg) 
 			rotateZ(${map[i][5]}deg) 
@@ -91,32 +103,53 @@ function createWorld(){
 
 createWorld();
 
-function update(){
-	// cos A   -sin A    x    x cos A - z sin A        
-	// 	               *   =     
-	// sin A    cos A    z    x sin A + z cos A
-	let xxx = right - left;
-	let zzz = forward - backward
-	dx =  xxx * Math.cos(me.ry*deg) - zzz * Math.sin(me.ry*deg);
-	dz =  -zzz * Math.cos(me.ry*deg) - xxx * Math.sin(me.ry*deg);
+function update() {
+	if (!gamePaused) {
+		// cos A   -sin A    x    x cos A - z sin A        
+		//               *   =     
+		// sin A    cos A    z    x sin A + z cos A
+		let xxx = right - left;
+		let zzz = forward - backward
+		let dx =  xxx * Math.cos(me.ry * deg) - zzz * Math.sin(me.ry * deg);
+		let dz =  -zzz * Math.cos(me.ry * deg) - xxx * Math.sin(me.ry * deg);
 
-	dry = - mouseX;
+		let dry = -mouseX;
+		mouseX = 0;
 
-	mouseX = 0;
+		me.z += dz;
+		me.x += dx;
 
-	me.z += dz;
-	me.x += dx;
+		if (lock) {
+			me.ry += dry;
+		}
 
-	if(lock){
-		me.ry += dry;
-	}
-
-	world.style.transform = `
-		translateZ(600px)
-		rotateY(${-me.ry}deg)
- 		translate3d(${-me.x}px, ${me.y}px, ${-me.z}px)
-		
+		world.style.transform = `
+			translateZ(600px)
+			rotateY(${-me.ry}deg)
+	 		translate3d(${-me.x}px, ${me.y}px, ${-me.z}px)
 		`;
+	}
 }
 
- timer = setInterval(update, 10);
+var timer = setInterval(update, 10);
+
+function activateMenu() {
+	gamePaused = !gamePaused;
+	if (gamePaused) {
+		menu.style.display = "block";
+		document.exitPointerLock();
+	} else {
+		menu.style.display = "none";
+		container.requestPointerLock();
+	}
+}
+
+function deactivateMenu() {
+	gamePaused = false;
+	menu.style.display = "none";
+	container.requestPointerLock();
+}
+
+function displayRules() {
+	rules.style.display = rules.style.display === "none" ? "block" : "none";
+}
